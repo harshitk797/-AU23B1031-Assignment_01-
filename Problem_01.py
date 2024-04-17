7139b90323c3678a6d0827c2ad3459dd34913c73
harshitk797 <165653613+harshitk797@users.noreply.github.com>
2024-04-17 13:03:29 +0000

    First commit

---
+++ Problem_01.py
@@ -0,0 +1,18 @@
+def get_name():
+    name=input("Enter your name : ")
+    return name
+
+def get_tshirt(brand_name):
+    user_name = get_name()
+    
+    Brands=[]
+    for i in range(int(input("Total no of brand Avalable :"))):
+        Brands.append(input(f"Enter brand name {i}: "))
+        i=i+1
+    print("Available brands:", Brands)
+    n=input("Brand name you are Looking for : ")
+    if n in Brands:
+        print(f"Hi {user_name}, the brand you are looking for is available in our store.")
+    else:
+        print(f"Hi {user_name}, Unfortunately the brand you are looking for is not available in our store.")
+            
